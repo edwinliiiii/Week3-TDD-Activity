fc618b6b7d40233524685f5110bc07ee55f2260a
edwinliiiii <91173669+edwinliiiii@users.noreply.github.com>
2024-09-17 15:20:37 -0400

    done;

--- src/models/calculator.model.spec.ts
+++ src/models/calculator.model.spec.ts
@@ -1,15 +1,79 @@
-
 import { CalculatorModel } from './calculator.model';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
+import { NumericKeys } from '../enums/numeric-keys.enum';
+import { OperatorKeys } from '../enums/operator-keys.enum';
 
 describe('CalculatorModel', (): void => {
+  let calculator: ICalculatorModel;
 
-  it('should contain a CalculatorModel class that implements ICalculatorModel', (): void => {
-
-    const calculator: ICalculatorModel = new CalculatorModel();
-
-    expect(calculator).toBeDefined();
-
+  beforeEach((): void => {
+    calculator = new CalculatorModel();
   });
 
+  it('should contain a CalculatorModel class that implements ICalculatorModel', (): void => {
+    expect(calculator).toBeDefined();
+  });
+
+  it('should have an empty display on init', (): void => {
+    // Act
+    const displayValue: string = calculator.display();
+
+    // Assert
+    expect(displayValue).toEqual('');
+  });
+
+  it('should display `1` when the `1` key is pressed', (): void => {
+    // Act
+    calculator.pressNumericKey(NumericKeys.ONE);
+    const displayValue: string = calculator.display();
+
+    // Assert
+    expect(displayValue).toEqual('1');
+  });
+
+  it('should display `2` when the `2` key is pressed', (): void => {
+    calculator.pressNumericKey(NumericKeys.TWO);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('2');
+  });
+
+  it('should display `98` when the `9` key is pressed followed by the `8` key', (): void => {
+    calculator.pressNumericKey(NumericKeys.NINE);
+    calculator.pressNumericKey(NumericKeys.EIGHT);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('98');
+  });
+
+  it('should display `98+` when the buffer contains `98` and the `+` key is pressed', (): void => {
+    calculator.pressNumericKey(NumericKeys.NINE);
+    calculator.pressNumericKey(NumericKeys.EIGHT);
+    calculator.pressOperatorKey(OperatorKeys.PLUS);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('98 + ');
+  });
+
+  it('should display `98+` when the buffer contains `98` and the `+` key is pressed', (): void => {
+    calculator.pressNumericKey(NumericKeys.NINE);
+    calculator.pressNumericKey(NumericKeys.EIGHT);
+    calculator.pressOperatorKey(OperatorKeys.PLUS);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('98 + ');
+  });
+
+  it('should display `98+` when the buffer contains `98` and the `*` key is pressed', (): void => {
+    calculator.pressNumericKey(NumericKeys.NINE);
+    calculator.pressNumericKey(NumericKeys.EIGHT);
+    calculator.pressOperatorKey(OperatorKeys.MULT);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('98 * ');
+  });
+
+  it('should throw error when the buffer contains `` and the `+` key is pressed', (): void => {
+    expect(calculator.pressOperatorKey(OperatorKeys.PLUS)).toThrow('Operator no');
+  });
 });
--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -1,25 +1,75 @@
-
 import { ActionKeys } from '../enums/action-keys.enum';
 import { NumericKeys } from '../enums/numeric-keys.enum';
 import { OperatorKeys } from '../enums/operator-keys.enum';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
 
 export class CalculatorModel implements ICalculatorModel {
+  private _buffer: string = '';
 
   public pressNumericKey(key: NumericKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer = this._buffer + key.valueOf();
   }
 
+  // 234 + 234
   public pressOperatorKey(key: OperatorKeys): void {
-    throw new Error('Method not implemented.');
+    if (this._buffer.length === 0 || !(this._buffer.slice(-1) in Object.values(NumericKeys))) {
+      throw new Error('Operator no');
+    }
+
+    this._buffer = this._buffer + ' ' + key.valueOf() + ' ';
   }
 
   public pressActionKey(key: ActionKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer = this._buffer + ' ' + key.valueOf() + ' ';
+  }
+
+  private evaluate(): number {
+    const keyArr: string[] = this._buffer.split(' ');
+    const operatorMap: Map<String, () => number> = {
+      '+': (a: number, b: number) => a + b,
+      '-': (a: number, b: number) => a - b,
+      '*': (a: number, b: number) => a * b,
+      '/': (a: number, b: number) => a / b,
+    };
+
+    //
+    var simpleArr: string[] = [];
+    for (var i = 0; i < keyArr.length; i++) {
+      if (keyArr[i] === OperatorKeys.DIV || keyArr[i] === OperatorKeys.MULT) {
+        const val: number = operatorMap[keyArr[i]](parseInt(keyArr[i--]), parseInt(keyArr[i++]));
+        simpleArr.push(String(val));
+      } else {
+        simpleArr.push(keyArr[i]);
+      }
+
+      i++;
+    }
+
+    for (var i = 0; i < simpleArr.length; i++) {
+      if (simpleArr[i] === OperatorKeys.PLUS) {
+        operatorMap[OperatorKeys.PLUS](parseInt(simpleArr[i--]), parseInt(simpleArr[i++]));
+      } else if (simpleArr[i] === OperatorKeys.MINUS) {
+        operatorMap[OperatorKeys.MINUS](parseInt(simpleArr[i--]), parseInt(simpleArr[i++]));
+      }
+      i++;
+    }
+
+    return 1;
   }
 
   public display(): string {
-    throw new Error('Method not implemented.');
+    return this._buffer;
   }
-
 }
+
+// 2 + 3 - 1
+// 2 + 3 / 2 * 2 + 5
+
+// [5, ]
+
+// [2, +, 3, /, 2, *, 2, +, 5]
+// [2, +, 1, *, 2, +, 5]
+
+// for i in nums:
+//     if i === enum.DIVIS || i === enum.MULT:
+//
